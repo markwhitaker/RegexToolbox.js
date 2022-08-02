bfa5902946cdadb9848e879a8cabf616d4b0de25
Mark Whitaker <markw@bitrot.net>
2022-08-02 08:05:19 +0100

    Minor formatting change to index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import RegexOptions from './regex-options.js';
 import RegexQuantifier from './regex-quantifier.js';
 import RegexBuilder from './regex-builder.js';
 
-export { RegexOptions, RegexQuantifier, RegexBuilder }
+export {
+    RegexOptions,
+    RegexQuantifier,
+    RegexBuilder
+};
